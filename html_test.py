9412f1bd21161f9d01e79cdd8b7553ac9cd68f01
SharpdotEXE <69698890+SharpdotEXE@users.noreply.github.com>
2021-09-07 05:17:09 -0500

    Update html_test.py

--- html_test.py
+++ html_test.py
@@ -2,10 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 session = requests.Session()
-response = session.get(
-    'https://www.dell.com/support/home/en-us/product-support/servicetag/C60D5Q2/overview',
-    headers={'User-Agent': 'Chrome/93.0.4577.63',
-             'cache-control': 'no-cache'})
+response = session.get('https://www.dell.com/support/home/en-us/product-support/servicetag/C60D5Q2/overview',
+           headers={'User-Agent': 'Chrome/93.0.4577.63', 'cache-control': 'no-cache'})
 
 #print(response.status_code)
 
