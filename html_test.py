0905bb9a0a857b8d673d415366bd4627a72be643
SharpdotEXE <69698890+SharpdotEXE@users.noreply.github.com>
2021-09-07 05:15:23 -0500

    Add files via upload

---
+++ html_test.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+session = requests.Session()
+response = session.get(
+    'https://www.dell.com/support/home/en-us/product-support/servicetag/C60D5Q2/overview',
+    headers={'User-Agent': 'Chrome/93.0.4577.63',
+             'cache-control': 'no-cache'})
+
+#print(response.status_code)
+
+soup = BeautifulSoup(response.content, 'html.parser')
+#print(soup.prettify())
+html = list(soup.children)[0]
+
+print(list(html.children)[3])
+
+
